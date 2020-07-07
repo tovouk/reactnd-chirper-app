f2a396d181003b5d990f2366e5855315780ce549
Hectic Envek <burningremedy@gmail.com>
2020-07-07 18:08:13 -0500

    load Dashboard only if done loading

---
+++ src/components/Dashboard.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class Dashboard extends Component{
+
+    render(){
+        return (
+            <div>
+                <h3 className="center">Your Timeline</h3>
+                <ul className="dashboard-list">
+                    {this.props.tweetIds.map((id)=> (
+                        <li key={id}>TWEET ID: {id}</li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps ({tweets}) {
+    return {
+        tweetIds: Object.keys(tweets).sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
